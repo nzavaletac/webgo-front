77c43bf2dd478f724e51a404bbec1da8dbf20832
BraulioNR <branoleruiz@gmail.com>
2022-03-28 23:14:27 -0500

    delete icon price in Tags.js

--- src/components/home/components/Tags.js
+++ src/components/home/components/Tags.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { Container, Icon } from "./TagsElements";
-import price from "../../../assets/images/price.svg";
-import categories from "../../../assets/images/categories.svg";
-import date from "../../../assets/images/date.svg";
+import React from "react"
+import { Container, Icon } from "./TagsElements"
+import categories from "../../../assets/images/categories.svg"
+import date from "../../../assets/images/date.svg"
 
 const Tags = () => {
   return (
     <Container>
-      <Icon to="/">
-        <img src={price} alt="price" />
-      </Icon>
-      <p>Prince</p>
       <Icon to="/">
         <img src={categories} alt="categories" />
       </Icon>
@@ -20,7 +15,7 @@ const Tags = () => {
       </Icon>
       <p>Date</p>
     </Container>
-  );
-};
+  )
+}
 
-export default Tags;
+export default Tags
