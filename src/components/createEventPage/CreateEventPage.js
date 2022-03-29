4cb55577795435f2695713248e876d23308b512a
BraulioNR <branoleruiz@gmail.com>
2022-03-28 23:05:40 -0500

    delete datepicker timepicker in createEventPage

--- src/components/createEventPage/CreateEventPage.js
+++ src/components/createEventPage/CreateEventPage.js
@@ -16,18 +16,12 @@ import {
 } from "./CreateEventPageElements"
 import AdapterDateFns from "@mui/lab/AdapterDateFns"
 import LocalizationProvider from "@mui/lab/LocalizationProvider"
-import DatePicker from "@mui/lab/DatePicker"
 import TextField from "@mui/material/TextField"
-import TimePicker from "@mui/lab/TimePicker"
 import "mapbox-gl/dist/mapbox-gl.css"
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js"
 
 const CreateEventPage = () => {
   const [valueDate, setValueDate] = React.useState(new Date())
-  const [valueTime, setValueTime] = React.useState(
-    new Date("2018-01-01T00:00:00.000Z")
-  )
-  const [value, setValue] = React.useState(new Date())
   mapboxgl.accessToken = process.env.REACT_APP_MAP_TOKEN
   const mapContainer = useRef(null)
   const map = useRef(null)
@@ -49,35 +43,14 @@ const CreateEventPage = () => {
             <Label>Title</Label>
             <Input type="text" placeholder="Enter events's title " />
             <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Label>Date</Label>
-
-              <DatePicker
-                mask={"__/__/____"}
-                value={valueDate}
-                minDate={new Date("01-01-2020")}
-                onChange={(newValue) => {
-                  setValueDate(newValue)
-                }}
-                renderInput={(params) => (
-                  <TextField variant="standard" {...params} />
-                )}
-              />
-              <Label>Time</Label>
-              <TimePicker
-                value={valueTime}
-                onChange={setValueTime}
-                renderInput={(params) => (
-                  <TextField variant="standard" {...params} />
-                )}
-              />
               <Label>DateTime</Label>
               <DateTime
                 renderInput={(props) => (
                   <TextField variant="standard" {...props} />
                 )}
-                value={value}
+                value={valueDate}
                 onChange={(newValue) => {
-                  setValue(newValue)
+                  setValueDate(newValue)
                 }}
               />
             </LocalizationProvider>
