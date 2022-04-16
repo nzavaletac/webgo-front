804ac342b45148b8b56600498e0ca75f8453830a
BraulioNR <branoleruiz@gmail.com>
2022-04-16 09:58:45 -0500

    Update helperctegories

--- src/helpers/HelperCategories.js
+++ src/helpers/HelperCategories.js
@@ -1,9 +1,9 @@
 export default function HelperCategories() {
   const arrCategories = [
-    { title: "concert", id: 1 },
-    { title: "marathon", id: 2 },
-    { title: "sport", id: 3 },
-    { title: "opening", id: 4 },
+    { title: "concert", id: "1" },
+    { title: "marathon", id: "2" },
+    { title: "sport", id: "3" },
+    { title: "opening", id: "4" },
   ]
   return arrCategories
 }
