1e8eecc9351fd02c8119223dd3bd9ebdcca905c7
BraulioNR <branoleruiz@gmail.com>
2022-04-16 10:00:25 -0500

    Upload component CardEvent in EventsPage

--- src/components/events/EventsPage.js
+++ src/components/events/EventsPage.js
@@ -12,16 +12,45 @@ import {
   OptionSelect,
 } from "./EventsPageElements"
 import CardEvent from "./components/cardEvent/CardEvent.js"
-import HelperEvents from "../../helpers/HelperEvents"
 import HelperSortEvents from "../../helpers/HelperSortEvents.js"
+import { getListAll } from "../../services/Event.services"
 
 class Events extends React.Component {
   constructor() {
     super()
     this.state = {
       selectedId: 0,
-      arrEvents: HelperEvents(),
+      //arrEvents: HelperEvents(),
+      arrEvents: [
+        /*{
+          categories: [{}, {}],
+          date: new Date(),
+          description: "Concierto aniversario Agua Marina",
+          src: "https://cdn.joinnus.com/files/2022/01/drquhEGIghYW6LQ.jpg",
+          title: "Concierto Agua Marina",
+          _id: "6232de2ce4c2c2cbd4b722e3",
+        },*/
+      ],
     }
+  }
+
+  componentDidMount() {
+    let Carousel_1 = "https://cdn.joinnus.com/files/2022/01/drquhEGIghYW6LQ.jpg"
+    const arrCategories = [
+      { title: "concert", _id: 1 },
+      { title: "marathon", _id: 2 },
+      { title: "sport", _id: 3 },
+    ]
+    getListAll().then((data) => {
+      console.log("Data")
+      console.log(data)
+      data.map((dato) => {
+        dato.date = new Date(dato.date)
+        dato.src = Carousel_1
+        dato.categories = arrCategories
+      })
+      this.setState({ arrEvents: data })
+    })
   }
 
   dropdownChanged(e) {
@@ -61,13 +90,13 @@ class Events extends React.Component {
             </Select>
           </DivButtons>
           <DivEvents>
-            {this.state.arrEvents.map((event) => {
+            {this.state.arrEvents?.map((event) => {
               return (
                 <CardEvent
                   src={event.src}
                   alt={event.title}
-                  id={event.id}
-                  key={event.id}
+                  id={event._id}
+                  key={event._id}
                   title={event.title}
                   date={event.date}
                   categories={event.categories}
