cb68b5609a40cd527029f0dd8c230d289938471f
BraulioNR <branoleruiz@gmail.com>
2022-04-18 02:13:08 -0500

    Update and delete comments in HelperEvents.js

--- src/helpers/HelperEvents.js
+++ src/helpers/HelperEvents.js
@@ -1,11 +1,5 @@
 import { getListAll } from "../../src/services/Event.services.js"
 export default function HelperEvents() {
-  let Carousel_1 = "https://cdn.joinnus.com/files/2022/01/drquhEGIghYW6LQ.jpg"
-  const arrCategories = [
-    { title: "concert", _id: 1 },
-    { title: "marathon", _id: 2 },
-    { title: "sport", _id: 3 },
-  ]
   var arrEvents
   getListAll().then((data) => {
     arrEvents = data.events
