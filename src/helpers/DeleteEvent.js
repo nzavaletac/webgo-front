36876c5f5e84b64f33af14addda5af8ca536f81f
BraulioNR <branoleruiz@gmail.com>
2022-04-20 01:12:01 -0500

    Update herlpers: deleteEvent and HelperCategories

--- src/helpers/DeleteEvent.js
+++ src/helpers/DeleteEvent.js
@@ -1,3 +1,26 @@
+import Swal from "sweetalert2"
+import { deleteEvent } from "../services/Event.services"
+
 export default function DeleteEvent(id) {
+  deleteEvent(id).then((data) => {
+    if (data.event._id) {
+      Swal.fire({
+        title: "Successful",
+        text: "Event deleted successfully",
+        icon: "success",
+        timer: 3500,
+      })
+      window.location.href = "/events"
+    } else {
+      if (data.error) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          timer: 1500,
+        })
+      }
+    }
+  })
   return console.log("Eliminando evento: " + id)
 }
--- src/helpers/HelperCategories.js
+++ src/helpers/HelperCategories.js
@@ -1,9 +1,17 @@
-export default function HelperCategories() {
+/*export default function HelperCategories() {
   const arrCategories = [
-    { title: "concert", id: "1" },
-    { title: "marathon", id: "2" },
-    { title: "sport", id: "3" },
-    { title: "opening", id: "4" },
+    { title: "Concierto", id: "625a47b6fb0d138637b523c1" },
+    { title: "Maraton", id: "625a47befb0d138637b523c3" },
+    { title: "Deporte", id: "625b989d01de673fd504f5ec" },
   ]
   return arrCategories
+}
+*/
+import { getCategories } from "../../src/services/Category.services"
+export default function HelperCategories() {
+  var arrCategories = []
+  getCategories().then((data) => {
+    arrCategories = data.categories
+    return arrCategories
+  })
 }
